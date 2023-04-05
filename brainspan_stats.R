426a50ca33e0fe444420683e70b963c33ee026d2
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-05 14:08:47 +0100

    Update brainspan_stats.R

--- brainspan_stats.R
+++ brainspan_stats.R
@@ -1,4 +1,4 @@
-##This code reads in RBP expression data from Brainspan (shown is code for U2AF2), finds rows with "frontal cortex" in the brain_region column (for analysis of frontal cortex samples). It calculates the median and standard error for rows before and after row 239 (in a dataframe containing the sample names and RPKM values, row 239 is when the postnatal samples begins). It also performs a t-test to compare the two sets of data.
+##This code reads in RBP expression data from Brainspan (shown is code for U2AF2), finds rows with "frontal cortex" in the brain_region column (for analysis of frontal cortex samples). It checks normality using Shapiro-Wilk Test. It then calculates the median and interquartile range for the rows before row 239 (prenatal data) and after row 239 (postnatal data), and performs a Mann-Whitney U test for comparison. Although not used in the thesis, code for calculating mean and standard error, as well as conducting a t-test is also provided. 
 
 #read data (example is U2AF2 data from Brainspan)
 data <- read.csv("/home/a/aangeles/Downloads/U2AF2_data.csv")
@@ -7,11 +7,54 @@ head(data)
 fc_rows <- grep("frontal", data$brain_region)
 head(fc_rows)
 print(fc_rows)
-# calculate median and SE for "frontal cortex" rows before row 239
+
+library(nortest)
+# Subset the data for fetal and postnatal samples separately
+fetal_data <- data$RPKM[fc_rows[fc_rows < 239]]
+postnatal_data <- data$RPKM[fc_rows[fc_rows >= 239]]
+
+if (ad.test(fetal_data)$p.value < 0.05) {
+  print("Fetal data is not normally distributed")
+} else {
+  print("Fetal data is normally distributed")
+}
+
+# Test normality of postnatal data
+if (ad.test(postnatal_data)$p.value < 0.05) {
+  print("Postnatal data is not normally distributed")
+} else {
+  print("Postnatal data is normally distributed")
+}
+#subset for median and IQR calculation
+fetal <- data$RPKM[fc_rows[fc_rows < 239]]
+postnatal <- data$RPKM[fc_rows[fc_rows >= 239]]
+
+# Calculate median and IQR for each group
+fetal_med <- median(fetal)
+fetal_iqr <- IQR(fetal)
+postnatal_med <- median(postnatal)
+postnatal_iqr <- IQR(postnatal)
+
+# Print median and IQR for each group
+cat("Fetal group median:", fetal_med, "\n")
+cat("Fetal group IQR:", fetal_iqr, "\n")
+cat("Postnatal group median:", postnatal_med, "\n")
+cat("Postnatal group IQR:", postnatal_iqr, "\n")
+
+# Mann-Whitney U test
+mwu <- wilcox.test(fetal, postnatal)
+
+# Print test results
+cat("\nMann-Whitney U test:\n")
+cat("U statistic:", mwu$statistic, "\n")
+cat("p-value:", mwu$p.value, "\n")
+cat("Significant:", ifelse(mwu$p.value < 0.05, "Yes", "No"), "\n")
+
+# calculate mean and SE for "frontal cortex" rows before row 239
 prenatal_mean <- mean(data$RPKM[fc_rows[fc_rows < 239]])
 prenatal_se <- sd(data$RPKM[fc_rows[fc_rows < 239]]) / sqrt(length(fc_rows[fc_rows < 239]))
 
-# calculate median and SE for "frontal cortex" rows after row 239
+# calculate mean and SE for "frontal cortex" rows after row 239
 postnatal_mean <- mean(data$RPKM[fc_rows[fc_rows >= 239]])
 postnatal_se <- sd(data$RPKM[fc_rows[fc_rows >= 239]]) / sqrt(length(fc_rows[fc_rows >= 239]))
 
