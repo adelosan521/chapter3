47b8aaf3c5be48215baab6dac2eec4cc777d503f
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-17 22:05:35 +0100

    Update sampling.R

--- sampling.R
+++ sampling.R
@@ -2,19 +2,19 @@
 # read data  
 mydata <- read.table("/ceph/project/tunbridgelab/aangeles/conservation/phyloP17/phyloP_CACNA1C-exon20_21.txt", header=TRUE, sep="\t")
 
-# Sample 4 Gs and 4 Ts and compute mean conservation score (because BRUNOL4 / BRUNOL5 binding site is comprised of 4 Gs and 4 Ts)
+# Sample 4 Gs and 4 Ts and compute median conservation score (because BRUNOL4 / BRUNOL5 binding site is comprised of 4 Gs and 4 Ts)
 set.seed(123) # set seed for reproducibility
 results <- replicate(1000, {
   selected_indices <- c(sample(which(mydata$DNA_nucleotides == "G"), 4), 
                         sample(which(mydata$DNA_nucleotides == "T"), 4))
-  mean_score <- median(mydata$evolutionary_conservation_score[selected_indices])
-  c(mean_score, sd_score)
+  median_score <- median(mydata$evolutionary_conservation_score[selected_indices])
+  c(median_score, sd_score)
 })
 
 # Extract median values
-means <- results[seq(1, length(results), 2)]
+medians <- results[seq(1, length(results), 2)]
 # Compute percentiles
-percentiles <- quantile(means, c(0.025, 0.975))
+percentiles <- quantile(medians, c(0.025, 0.975))
 cat("2.5% percentile:", percentiles[1], "\n")
 cat("97.5% percentile:", percentiles[2], "\n")
 
@@ -22,9 +22,9 @@ cat("97.5% percentile:", percentiles[2], "\n")
 median_score <- median(mydata$evolutionary_conservation_score)
 cat("Median evolutionary score:", median_score, "\n")
 
-# Plot histogram with mean and percentile lines
-hist(means, breaks=50, xlab="Median conservation score", main="Distribution of median conservation score")
-abline(v=mean(means), col="red", lwd=2)
+# Plot histogram with median and percentile lines
+hist(medians, breaks=50, xlab="Median conservation score", main="Distribution of median conservation score")
+abline(v=mean(medians), col="red", lwd=2)
 abline(v=percentiles[1], col="blue", lwd=2, lty="dashed")
 abline(v=percentiles[2], col="blue", lwd=2, lty="dashed")
 legend("topleft", legend=c("Median", "2.5% percentile", "97.5% percentile"), col=c("red", "blue", "blue"), lty=c(1, 1, 1), lwd=1)
