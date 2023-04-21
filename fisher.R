78ef5af41f70463d682f5b5c557b9bd442375930
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-22 00:51:36 +0100

    Update fisher.R

--- fisher.R
+++ fisher.R
@@ -1,42 +1,22 @@
 ## This script was used to conduct a Fisher's exact test to compare FIMO and HOMER for five different RBPs (ELAVL1, KHDRBS1, TARDBP, PABPC4, FXR1). It produces a table that is saved to a PNG image file.
-## Create a data frame of the frequencies for FIMO and HOMER
-frequencies <- data.frame(
-  Method = rep(c("FIMO", "HOMER"), each = 5),
-  RBP = c("ELAVL1", "KHDRBS1", "TARDBP", "PABPC4", "FXR1"),
-  Observed_FIMO = c(6, 2, 5, 7, 0),
-  Not_Observed_FIMO = c(24, 28, 25, 23, 30),
-  Observed_HOMER = c(20, 5, 11, 11, 1),
-  Not_Observed_HOMER = c(10, 25, 19, 19, 29)
-)
+## Below is the code to conduct Fisher's Exact Test for comparing ELAVL1 frequency between FIMO and HOMER
+# Define the observed counts
+observed_counts <- matrix(c(6, 24, 20, 10), nrow = 2)
 
-## Create an empty data frame to store the results
-results <- data.frame(RBP = character(5), FIMO = character(5), HOMER = character(5), 
-                      FishersExact = numeric(5), PValue = character(5))
+# Define the row and column names
+rownames(observed_counts) <- c("FIMO", "HOMER")
+colnames(observed_counts) <- c("ELAVL1 present", "ELAVL1 absent")
 
-## Perform a Fisher's exact test for each RBP and fill in the results data frame
-for (i in 1:5) {
-  table <- matrix(c(frequencies[i, "Observed_FIMO"], frequencies[i, "Observed_HOMER"],
-                    frequencies[i, "Not_Observed_FIMO"], frequencies[i, "Not_Observed_HOMER"]),
-                  nrow = 2, byrow = TRUE)
-  colnames(table) <- c("Observed", "Not Observed")
-  rownames(table) <- c("FIMO", "HOMER")
-  test <- fisher.test(table)
-  results[i, "RBP"] <- frequencies[i, "RBP"]
-  results[i, "FIMO"] <- paste(frequencies[i, "Observed_FIMO"], "/", frequencies[i, "Observed_FIMO"] + frequencies[i, "Not_Observed_FIMO"])
-  results[i, "HOMER"] <- paste(frequencies[i, "Observed_HOMER"], "/", frequencies[i, "Observed_HOMER"] + frequencies[i, "Not_Observed_HOMER"])
-  results[i, "FishersExact"] <- format(test$estimate, digits = 4)
-  results[i, "PValue"] <- format.pval(test$p.value, digits = 4)
+# Perform the Fisher's Exact Test
+result <- fisher.test(observed_counts)
+
+# Print the odds ratio and p-value
+cat("Odds ratio: ", result$estimate, "\n")
+cat("p-value: ", result$p.value, "\n")
+
+# Check for significance at 0.05 level (i.e. alpha is set at 0.05)
+if(result$p.value < 0.05){
+  cat("The difference in ELAVL1 frequency between FIMO and HOMER is significant.\n")
+}else{
+  cat("There is no significant difference in ELAVL1 frequency between FIMO and HOMER.\n")
 }
-
-## Print the results table
-cat("RBP\tFIMO\tHOMER\tFisher's Exact Test\tp-value\n")
-for (i in 1:5) {
-  cat(paste0(results[i, "RBP"], "\t", results[i, "FIMO"], "\t", 
-             results[i, "HOMER"], "\t", results[i, "FishersExact"], "\t", 
-             results[i, "PValue"], "\n"))
-}
-
-## Save the results table as a PNG file
-png(file = "/home/a/aangeles/Downloads/results3.png", width = 800, height = 600)
-grid.table(as.matrix(results), theme = ttheme_default(base_size = 10))
-dev.off()
