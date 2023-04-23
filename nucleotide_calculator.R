969399c7366c4b1bdfd2e11ff315aa9b3d6a877c
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-23 23:15:04 +0100

    Update nucleotide_calculator.R

--- nucleotide_calculator.R
+++ nucleotide_calculator.R
@@ -1,25 +1,24 @@
 ## This script is code for calculating nucleotide frequencies in a given nucleotide sequence. In this thesis, this script was used to calculate nucleotide frequency of VGCC splicing regions of interest.
 # Create a function to calculate nucleotide frequencies
 calculate_frequencies <- function(sequence) {
-  # Convert the sequence to uppercase
-  sequence <- toupper(sequence)
+# Convert the sequence to uppercase
+sequence <- toupper(sequence)
   
 # Count the occurrences of each nucleotide A, C, G, and T separately
-  a_count <- sum(grepl("A", strsplit(sequence, "")[[1]]))
-  c_count <- sum(grepl("C", strsplit(sequence, "")[[1]]))
-  g_count <- sum(grepl("G", strsplit(sequence, "")[[1]]))
-  t_count <- sum(grepl("T", strsplit(sequence, "")[[1]]))
+a_count <- sum(grepl("A", strsplit(sequence, "")[[1]]))
+c_count <- sum(grepl("C", strsplit(sequence, "")[[1]]))
+g_count <- sum(grepl("G", strsplit(sequence, "")[[1]]))
+t_count <- sum(grepl("T", strsplit(sequence, "")[[1]]))
   
 # Calculate the frequencies of A, C, G, and T separately
-  total_count <- a_count + c_count + g_count + t_count
-  a_frequency <- a_count / total_count
-  c_frequency <- c_count / total_count
-  g_frequency <- g_count / total_count
-  t_frequency <- t_count / total_count
+total_count <- a_count + c_count + g_count + t_count
+a_frequency <- a_count / total_count
+c_frequency <- c_count / total_count
+g_frequency <- g_count / total_count
+t_frequency <- t_count / total_count
   
 # Create a list to store the frequencies
-  frequencies <- list(A = a_frequency, C = c_frequency, G = g_frequency, T = t_frequency)
-  
+frequencies <- list(A = a_frequency, C = c_frequency, G = g_frequency, T = t_frequency) 
   return(frequencies)
 }
 
