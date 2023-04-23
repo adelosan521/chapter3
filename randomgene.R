a695bcc65999ac95e4242ba809ddeb168c418df8
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-24 00:02:16 +0100

    Update randomgene.R

--- randomgene.R
+++ randomgene.R
@@ -1,22 +1,21 @@
 ##This script randomly selects genes from the human genome. Example shown is for randomly selecting ten genes.
 
-## load required libraries 
 library(tidyverse)
 library(biomaRt)
 
-# connect to the Ensembl database and select the human genome
-ensembl = useMart("ensembl")
-ensembl = useDataset("hsapiens_gene_ensembl", mart = ensembl)
+# Establish a connection to the Ensembl database and choose the human genome
+mart <- useMart("ensembl")
+mart <- useDataset("hsapiens_gene_ensembl", mart = mart)
 
-# retrieve all gene IDs and names
-genes = getBM(attributes = c("ensembl_gene_id", "external_gene_name"), mart = ensembl)
+# Obtain all gene IDs and names
+all_genes <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"), mart = mart)
 
-# randomly select 10 genes
-n_genes = 10
-random = genes %>% sample_n(n_genes)
+# Select 10 random genes
+num_genes <- 10
+random_genes <- all_genes %>% sample_n(num_genes)
 
-# print the selected genes
+# Display the selected genes
 cat("Randomly selected genes:\n")
-for (i in 1:n_genes) {
-  cat(paste(i, ".", random[i, "external_gene_name"], "(", random[i, "ensembl_gene_id"], ")\n"))
+for (i in seq_len(num_genes)) {
+  cat(paste(i, ".", random_genes[i, "external_gene_name"], "(", random_genes[i, "ensembl_gene_id"], ")\n"))
 }
