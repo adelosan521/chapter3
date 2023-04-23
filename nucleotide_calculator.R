adf020a0f652b8930cd106e49533c07da70b2d01
Alejandro De Los Angeles <72091611+adelosan521@users.noreply.github.com>
2023-04-24 00:43:49 +0100

    Update nucleotide_calculator.R

--- nucleotide_calculator.R
+++ nucleotide_calculator.R
@@ -1,28 +1,28 @@
 ## This script is code for calculating nucleotide frequencies in a given nucleotide sequence. In this thesis, this script was used to calculate nucleotide frequency of VGCC splicing regions of interest.
-# Create a function to calculate nucleotide frequencies
-calculate_frequencies <- function(sequence) {
-# Convert the sequence to uppercase
-sequence <- toupper(sequence)
-  
-# Count the occurrences of each nucleotide A, C, G, and T separately
-a_count <- sum(grepl("A", strsplit(sequence, "")[[1]]))
-c_count <- sum(grepl("C", strsplit(sequence, "")[[1]]))
-g_count <- sum(grepl("G", strsplit(sequence, "")[[1]]))
-t_count <- sum(grepl("T", strsplit(sequence, "")[[1]]))
-  
-# Calculate the frequencies of A, C, G, and T separately
-total_count <- a_count + c_count + g_count + t_count
-a_frequency <- a_count / total_count
-c_frequency <- c_count / total_count
-g_frequency <- g_count / total_count
-t_frequency <- t_count / total_count
-  
-# Create a list to store the frequencies
-frequencies <- list(A = a_frequency, C = c_frequency, G = g_frequency, T = t_frequency) 
-  return(frequencies)
+# Define a function to compute nucleotide frequencies
+compute_frequencies <- function(seq) {
+  # Change the sequence to uppercase
+  seq <- toupper(seq)
+
+  # Determine the counts of each nucleotide A, C, G, and T individually
+  count_a <- sum(grepl("A", strsplit(seq, "")[[1]]))
+  count_c <- sum(grepl("C", strsplit(seq, "")[[1]]))
+  count_g <- sum(grepl("G", strsplit(seq, "")[[1]]))
+  count_t <- sum(grepl("T", strsplit(seq, "")[[1]]))
+
+  # Compute the frequencies of A, C, G, and T individually
+  total_count <- count_a + count_c + count_g + count_t
+  freq_a <- count_a / total_count
+  freq_c <- count_c / total_count
+  freq_g <- count_g / total_count
+  freq_t <- count_t / total_count
+
+  # Store the frequencies in a list
+  freq_list <- list(A = freq_a, C = freq_c, G = freq_g, T = freq_t)
+  return(freq_list)
 }
 
 # Example usage (sequence is "ACGTAGAGAGAGTAC")
-sequence <- "ACGTAGAGAGAGTAC"
-frequencies <- calculate_frequencies(sequence)
-print(frequencies)
+input_sequence <- "ACGTAGAGAGAGTAC"
+resulting_frequencies <- compute_frequencies(input_sequence)
+print(resulting_frequencies)
